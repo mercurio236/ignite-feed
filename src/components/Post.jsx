c20112b93ba8998ee947e42c831a723f0db41967
Arley Souto <macx15@hotmail.com>
2023-08-26 15:43:54 -0300

    finalizado post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,11 +29,18 @@ export function Post() {
         </p>
         <p>
           {" "}
-          <a href=""> #novoprojeto</a>{' '}
-          <a href="">#nlw </a>{' '}
-          <a href="">#rocketseat</a>{' '}
+          <a href=""> #novoprojeto</a> <a href="">#nlw </a>{" "}
+          <a href="">#rocketseat</a>{" "}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
