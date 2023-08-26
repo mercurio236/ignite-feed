986fc11623b9d1f9509f1f46d34eb17dff448d78
Arley Souto <macx15@hotmail.com>
2023-08-26 17:48:39 -0300

    criando component de avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img src="https://github.com/mercurio236.png" />
+          <Avatar src="https://github.com/mercurio236.png" />
           <div className={styles.authorInfo}>
             <strong>Arley Souto</strong>
             <span>Software Enginer</span>
